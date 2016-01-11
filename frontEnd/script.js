b0b536bb8a7237cf184c54eed1518a567edd05a1
Hawkinou <clementaudry@gmail.com>
2016-01-11 11:07:56 +0100

    Initial Commit

---
+++ frontEnd/script.js
@@ -0,0 +1,144 @@
+/**
+ * VAR
+ */
+
+var e = null;
+var winHeigth = screen.height;
+var winWidth = screen.width;
+
+var json;
+
+var postIt = null;
+var listPostIt;
+var listDisplayedPostIt;
+
+/**
+ * WEB
+ */
+
+var connectionWs = function () {
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+    var connection = new WebSocket('ws://127.0.0.1:8080');
+
+    connection.onopen = function () {
+        // connection is opened and ready to use
+    };
+
+    connection.onerror = function (error) {
+        // an error occurred when sending/receiving data
+    };
+
+    connection.onmessage = function (message) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+            json = JSON.parse(message.data);
+            update();
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+        // handle incoming message
+    };
+};
+
+/**
+ * FCT
+ */
+
+function resize(coo, axis) {
+    "use strict";
+    console.log(coo*1+1);
+    return ((coo*1+1) * axis) / 2;
+}
+
+function editHand(r, l) {
+    "use strict";
+    var handR = document.getElementById("rightHand"),
+        handL = document.getElementById("leftHand");
+    handR.style.left = resize(r.x, winWidth);
+    handR.style.top = resize((r.y*-1), winHeigth);
+    handL.style.left = resize(l.x, winWidth);
+    handL.style.top = resize((l.y*-1), winHeigth);
+    console.log(handL.style.top + " " + handL.style.left + " " + handL.style.top + " " + handL.style.left);
+}
+
+function addToNotif(p) {
+    "use strict";
+    document.getElementById("notif").innerHTML += 1;
+    listPostIt.push(p);
+}
+
+function displayPostIt(p) {
+    "use strict";
+    postIt = document.createElement("div");
+    postIt.innerHTML = p.content;
+    postIt.setAttribute("id", p.id);
+    postIt.setAttribute("class", "postIt imported");
+    postIt.style.left = resize(p.x, winWidth);
+    postIt.style.top = resize(p.y, winHeigth);
+    document.body.insertBefore(postIt, document.getElementById("buttons"));
+}
+
+function editPostIt(p) {
+    "use strict";
+    postIt = document.getElementById(p.id);
+    postIt.innerHTML = p.content;
+    postIt.style.left = resize(p.x, winWidth);
+    postIt.style.top = resize(p.y, winHeigth);
+}
+
+function destroyPostIt(p) {
+    "use strict";
+    postIt = document.getElementById(p.id);
+    document.body.removeChild(postIt);
+    listDisplayedPostIt.splice(listDisplayedPostIt.indexOf(p), 1);
+    listPostIt.splice(listPostIt.indexOf(p), 1);
+}
+
+function openNotif() {
+    "use strict";
+    document.getElementById("importZone").style.display = "block";
+    document.getElementById("notif").className += "validate";
+    document.getElementById("notif").innerHTML = "OK";
+}
+
+function validateImport() {
+    "use strict";
+    document.getElementById("importZone").style.display = "none";
+    document.getElementById("notif").classList.remove(document.getElementById("notif").classList.indexOf("validate"));
+    document.getElementById("notif").innerHTML = 0;
+}
+
+function update() {
+    "use strict";
+    /*
+    // ajout postIt 
+    if (json.postIt.length > listPostIt.length){
+        document.getElementById("notif").innerHTML = json.postIt.length - listPostIt.length;
+        for (e in json.postIt){
+            if (listPostIt.indexOf(json.postIt[e]) == -1){
+                listPostIt.push(json.postIt[e]);
+            }
+        }
+    }
+    
+    // suppression postIt
+    if (json.postIt.length < listDisplayedPostIt.length){
+        for (e in listDisplayedPostIt){
+            if (listDisplayedPostIt.indexOf(json.postIt[e]) == -1){
+                
+            }
+        }
+    }*/
+    
+    // mettre a jour les mains
+    editHand(json.rightHand, json.leftHand);
+    
+}
+
+function start() {
+    "use strict";
+    connectionWs();
+}
---
+++ serveur.js
@@ -0,0 +1,110 @@
+var http = require('http');
+var webSocketServer = require('websocket').server;
+var url = require('url');
+var textejson='{"rightHand" :{"x" : 1,"y" : 1},"leftHand" :{"x" : -1,"y" : -1},"postIt" :[]}';
+var jsonObject = JSON.parse(textejson);
+var hand = true // Main droite = true Main gauche false
+var postItSelected;
+var connection;
+var server = http.createServer(function(req, res) {
+	var page = url.parse(req.url).pathname;
+	if (page=='/'){
+
+	}
+	else if(page=='/newPost'){
+		var body;
+		req.on('data', function (chunk) {
+			body = chunk;
+		});
+		req.on('end', function () {
+			console.log('POSTed: ' + body);
+			createPostIt(""+body);
+		});
+	}
+	else if(page=='/favicon.ico');
+	else{
+		var handValue=getHandPosition(page);
+		jsonObject.rightHand.x=handValue[1];
+		jsonObject.rightHand.y=handValue[2];
+		jsonObject.leftHand.x=handValue[3];
+		jsonObject.leftHand.y=handValue[4];
+		if (postItSelected){
+			jsonObject.postIt[postItSelected].x=handValue[1];
+			jsonObject.postIt[postItSelected].y=handValue[2];
+		}
+		if (Math.abs(handValue[1]*1-handValue[3]*1)<0.15&&Math.abs(handValue[2]*1-handValue[4]*1)<0.15&&inSelection){
+			if (postItSelected){
+				postItSelected=undefined;
+			}
+			else {
+				getSelectedPostIt(handValue);
+			}
+			inSelection=true;
+		}
+		else{
+			inSelection=false;
+		}
+		if (connection)
+			connection.sendUTF(JSON.stringify( jsonObject ));
+		console.log(JSON.stringify( jsonObject ));
+		res.end();
+
+	}
+});
+var wsServer = new webSocketServer({
+	// WebSocket server is tied to a HTTP server. WebSocket request is just
+	// an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
+	httpServer: server
+});
+wsServer.on('request', function(request) {
+	console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+	// accept connection - you should check 'request.origin' to make sure that
+	// client is connecting from your website
+	// (http://en.wikipedia.org/wiki/Same_origin_policy)
+	connection = request.accept(null, request.origin);
+	console.log((new Date()) + ' Connection accepted.');
+	connection.sendUTF(JSON.stringify( jsonObject ));
+
+
+	// user disconnected
+	connection.on('close', function(connection) {
+		console.log((new Date()) + " Peer "
+		+ connection.remoteAddress + " disconnected.");
+	});
+
+});
+var getSelectedPostIt= function(handValue){
+	if(hand){
+		function coord(element, index, array) {
+			if (handValue[1]>element.x&&handValue[1]<element.x+element.width){
+				if (handValue[2]>element.y&&handValue[2]<element.y+element.height){
+					postItSelected=index;
+				}
+			}
+		}
+		jsonObject.postIt.forEach(coord);
+	}
+	else{
+
+	}
+}
+var getHandPosition = function(url) {
+	var listCoord=url.split("/");
+	var handValue=[];
+	function coord(element, index, array) {
+		handValue[index]=element.replace(",",".");
+	}
+	listCoord.forEach(coord);
+	return handValue;
+};
+var createPostIt = function(data) {
+	var postIt={};
+	postIt.content=data;
+	postIt.x=0;
+	postIt.y=0;
+	postIt.width=0;
+	postIt.height=0;
+	jsonObject.postIt.push(postIt)
+};
+server.listen(8080);
